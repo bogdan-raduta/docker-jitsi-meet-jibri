8f063c8f65eb35c228eca1e4807c91f925a7e609
Bogdan Raduta <bogdan@u-need.io>
2019-02-13 15:13:09 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,43 @@
+FORCE_REBUILD ?= 0
+JITSI_RELEASE ?= "stable"
+JITSI_BUILD ?= "latest"
+
+ifeq ($(FORCE_REBUILD), 1)
+  BUILD_ARGS = "--no-cache"
+endif
+
+build-all:
+	BUILD_ARGS=$(BUILD_ARGS) JITSI_RELEASE=$(JITSI_RELEASE) $(MAKE) -C base build
+	BUILD_ARGS=$(BUILD_ARGS) $(MAKE) -C base-java build
+	BUILD_ARGS=$(BUILD_ARGS) $(MAKE) -C web build
+	BUILD_ARGS=$(BUILD_ARGS) $(MAKE) -C prosody build
+	BUILD_ARGS=$(BUILD_ARGS) $(MAKE) -C jicofo build
+	BUILD_ARGS=$(BUILD_ARGS) $(MAKE) -C jvb build
+	BUILD_ARGS=$(BUILD_ARGS) $(MAKE) -C jigasi build
+	BUILD_ARGS=$(BUILD_ARGS) $(MAKE) -C jibri build
+
+tag-all:
+	docker tag jitsi/base:latest jitsi/base:$(JITSI_BUILD)
+	docker tag jitsi/base-java:latest jitsi/base-java:$(JITSI_BUILD)
+	docker tag jitsi/web:latest jitsi/web:$(JITSI_BUILD)
+	docker tag jitsi/prosody:latest jitsi/prosody:$(JITSI_BUILD)
+	docker tag jitsi/jicofo:latest jitsi/jicofo:$(JITSI_BUILD)
+	docker tag jitsi/jvb:latest jitsi/jvb:$(JITSI_BUILD)
+	docker tag jitsi/jigasi:latest jitsi/jigasi:$(JITSI_BUILD)
+
+push-all:
+	docker push jitsi/base
+	docker push jitsi/base-java
+	docker push jitsi/web
+	docker push jitsi/prosody
+	docker push jitsi/jicofo
+	docker push jitsi/jvb
+	docker push jitsi/jigasi
+	docker push jitsi/jibri
+
+clean:
+	docker-compose stop
+	docker-compose rm
+	docker network prune
+
+.PHONY: build-all tag-all push-all clean
---
+++ prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
@@ -0,0 +1,66 @@
+admins = { "{{ .Env.JICOFO_AUTH_USER }}@{{ .Env.XMPP_AUTH_DOMAIN }}" }
+plugin_paths = { "/prosody-plugins-custom" }
+
+VirtualHost "{{ .Env.XMPP_DOMAIN }}"
+    {{ if .Env.ENABLE_AUTH | default "0" | toBool }}
+    authentication = "internal_plain"
+    {{ else }}
+    authentication = "anonymous"
+    {{ end }}
+    ssl = {
+        key = "/config/certs/{{ .Env.XMPP_DOMAIN }}.key";
+        certificate = "/config/certs/{{ .Env.XMPP_DOMAIN }}.crt";
+    }
+    modules_enabled = {
+        "bosh";
+        "pubsub";
+        "ping";
+        {{ if .Env.XMPP_MODULES }}
+        "{{ join "\";\n\"" (splitList "," .Env.XMPP_MODULES) }}";
+        {{ end }}
+    }
+
+    c2s_require_encryption = false
+
+{{ if and (.Env.ENABLE_AUTH | default "0" | toBool) (.Env.ENABLE_GUESTS | default "0" | toBool) }}
+VirtualHost "{{ .Env.XMPP_GUEST_DOMAIN }}"
+    authentication = "anonymous"
+    c2s_require_encryption = false
+{{ end }}
+
+VirtualHost "{{ .Env.XMPP_AUTH_DOMAIN }}"
+    ssl = {
+        key = "/config/certs/{{ .Env.XMPP_AUTH_DOMAIN }}.key";
+        certificate = "/config/certs/{{ .Env.XMPP_AUTH_DOMAIN }}.crt";
+    }
+    authentication = "internal_plain"
+
+{{ if .Env.XMPP_RECORDER_DOMAIN }}
+VirtualHost "{{ .Env.XMPP_RECORDER_DOMAIN }}"
+    modules_enabled = {
+      "ping";
+    }
+    authentication = "internal_plain"
+{{ end }}
+
+Component "{{ .Env.XMPP_INTERNAL_MUC_DOMAIN }}" "muc"
+    modules_enabled = {
+        "ping";
+        {{ if .Env.XMPP_INTERNAL_MUC_MODULES }}
+        "{{ join "\";\n\"" (splitList "," .Env.XMPP_INTERNAL_MUC_MODULES) }}";
+        {{ end }}
+    }
+    storage = "internal"
+    muc_room_cache_size = 1000
+
+Component "{{ .Env.XMPP_MUC_DOMAIN }}" "muc"
+    storage = "internal"
+    modules_enabled = {
+        {{ if .Env.XMPP_MUC_MODULES }}
+        "{{ join "\";\n\"" (splitList "," .Env.XMPP_MUC_MODULES) }}";
+        {{ end }}
+    }
+
+Component "focus.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JICOFO_COMPONENT_SECRET }}"
+
